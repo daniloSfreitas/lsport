ff509ac3f23ce75839ec197ee0f7a5d5ab29c1e4
paulohenriquefreitas <paulohfreitas_ti@hotmail.com>
2019-08-12 14:35:36 -0300

    DAO

---
+++ src/main/java/br/com/lsport/LsportApplication.java
@@ -0,0 +1,13 @@
+package br.com.lsport;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LsportApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LsportApplication.class, args);
+	}
+
+}
---
+++ src/main/java/br/com/lsport/controller/HelloControler.java
@@ -0,0 +1,21 @@
+package br.com.lsport.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+import br.com.lsport.model.User;
+
+@Controller
+public class HelloControler {
+
+    @GetMapping("/signup")
+    public ModelAndView showSignUpForm() {
+        User user = new User();
+        user.setName("Danilo");
+        ModelAndView m = new ModelAndView("index");
+        m.addObject(user);
+        return m;
+    }
+
+}
